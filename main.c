879e5f763408c498fb230f12542b9ebe6893b33b
Ayran Andrade <ayranandrade@hotmail.com>
2018-09-30 16:06:23 -0300

    Adiciona teste para a tabela de espalhamento

--- main.c
+++ main.c
@@ -1,8 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "lib/lista_ligada.h"
+#include "lib/tabela_espalhamento.h"
 
 int main (int argc, char** argv) {
 
-    printf("Hello world\n");
+    printf("Testando a tabela de espalhamento.\n");
+    TabSim* t = cria(10);
+
+    printf("Criando elementos para o teste.\n");
+    Elemento* e1 = (Elemento*)malloc(sizeof(Elemento*));
+    strcpy(e1->n, "e1");
+
+    Elemento* e2 = (Elemento*)malloc(sizeof(Elemento*));
+    strcpy(e2->n, "e2");    
+
+    printf("Inserindo os elementos criados.\n");
+    if (insere(t, "e1", e1) != 0) {
+        printf("PRIMEIRO ELEMENTO NÂO FOI INSERIDO!\n");
+    }
+    if (insere(t, "e2", e2) != 0) {
+        printf("SEGUNDO ELEMENTO NÂO FOI INSERIDO!\n");
+    }
+
+    printf("Buscando um elemento.\n");
+    Elemento* buscado = busca(t, "e1");
+    printf("Exibindo o valor do elemento buscado: %s\n", buscado->n);
+
+    printf("Retirando um elemento.\n");
+    if (retira(t, "e1") != 0) {
+        printf("PRIMEIRO ELEMENTO NÂO FOI REMOVIDO!\n");
+    }
+
+    printf("Destruindo a tabela de espalhamento.\n");
+    destroi(t);
+
+    printf("Terminei de testar a tabela de espalhamento.\n");
 
     return 0;
 }
